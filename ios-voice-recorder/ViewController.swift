a36650116ec02060e41a6c7cda03a05251440c5f
Abdullah-MGK <abdullah_mgk@outlook.com>
2020-04-03 11:53:48 +0300

    connect to ViewController, end lesson 2

--- ios-voice-recorder/ViewController.swift
+++ ios-voice-recorder/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var RecordBTN: UIButton!
+    
+    @IBOutlet weak var RecordLBL: UILabel!
+    
+    var recordIsTapped = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func recordTapped(_ sender: Any) {
+        if !recordIsTapped {
+            RecordLBL.text = "Recording ..."
+            RecordBTN.setTitle("Stop", for: .normal)
+            RecordBTN.backgroundColor = .darkGray
+            recordIsTapped = true
+        }
+        else {
+            RecordLBL.text = "Tap to Record"
+            RecordBTN.setTitle("Record", for: .normal)
+            RecordBTN.backgroundColor = .systemPink
+            recordIsTapped = false
+        }
+    }
 }
-
