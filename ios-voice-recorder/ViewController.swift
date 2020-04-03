6b90fc21e018b93bf630559576dc14ccec8e446c
Abdullah-MGK <abdullah_mgk@outlook.com>
2020-04-03 10:50:47 +0300

    add project files

---
+++ ios-voice-recorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-voice-recorder
+//
+//  Created by Abdullah Khayat on 4/3/20.
+//  Copyright © 2020 TeamIos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
