5a2d504b88e92a7ed2798861388f572e24d7ed73
Abdullah-MGK <abdullah_mgk@outlook.com>
2020-04-04 07:39:05 +0300

    add extension to playviewcontroller

--- ios-voice-recorder/PlayViewController.swift
+++ ios-voice-recorder/PlayViewController.swift
@@ -7,32 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayViewController: UIViewController {
     
     var recordedAudioURL: URL!
-    
+    var audioFile : AVAudioFile!
+    var audioEngine : AVAudioEngine!
+    var audioPlayerNode : AVAudioPlayerNode!
+    var stopTimer : Timer!
     
     @IBOutlet weak var slowBTN: UIButton!
-    
     @IBOutlet weak var fastBTN: UIButton!
-    
     @IBOutlet weak var lowpitchBTN: UIButton!
-    
     @IBOutlet weak var fastpitchBTN: UIButton!
-    
     @IBOutlet weak var echoBTN: UIButton!
-    
     @IBOutlet weak var reverbBTN: UIButton!
-    
     @IBOutlet weak var stopBTN: UIButton!
-    
     @IBOutlet weak var playBTN: UIButton!
-     
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, lowpitch, fastpitc, echo, reverb, stop, play
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        //configureUI(.notPlaying)
     }
     
     @IBAction func play(_ sender: UIButton) {
@@ -42,15 +46,15 @@ class PlayViewController: UIViewController {
     @IBAction func stop(_ sender: UIButton) {
         
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
