0666f573824f1d45bf7128791ec6340819bac9bb
Abdullah-MGK <abdullah_mgk@outlook.com>
2020-05-08 10:18:51 +0300

    update

--- ios-voice-recorder/PlayViewController.swift
+++ ios-voice-recorder/PlayViewController.swift
@@ -76,16 +76,3 @@ class PlayViewController: UIViewController {
      */
     
 }
-
-/*
- // https://classroom.udacity.com/courses/ud1025/lessons/f4db5826-41f1-43b7-9e2b-06a1416bb23b/concepts/5155ae8e-04bd-4baf-92f2-2da926242f42
- // When or Why delegate ...
- You probably noticed that try as you might, you couldn't get the silly song lyrics to display after putting in a name! There’s one final piece missing—there’s no obvious way to get out of the name field once you’ve typed in a name. We could add a button to kick things off, but what would feel best is to just type in our name and press Return, hiding the keyboard and displaying the new lyrics. We can respond to the user pressing Return by making our view controller conform to the UITextFieldDelegate protocol.
- 
- // How delegate work ...
- Note: UIKit uses the delegate pattern frequently to allow the same classes to exhibit different behaviors in different contexts. Rather than having to write your own text field class with specific behaviors when a user types a new character or begins or ends editing, adding a delegate to a system-provided text field lets you easily respond to those events.
- 
- // Organize view controllers using extensions
- Swift extensions are a handy way to organize the parts of our class that act as delegates or data sources. Create a ViewController extension that adds conformance to UITextFieldDelegate. The delegate method we want to supply is called textFieldShouldReturn and is called whenever a user presses the return key inside a text field.
- */
- 
